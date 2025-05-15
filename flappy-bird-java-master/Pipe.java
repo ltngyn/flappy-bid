e3784d8cce8c4c3db421945275be1bea9463b512
Zerzo15 <vvmquang1510@gmail.com>
2025-05-15 23:39:24 +0700

    Create Pipe.java

---
+++ flappy-bird-java-master/Pipe.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class Pipe {
+    public int x, y, width, height;
+    public Image img;
+    public boolean passed = false;
+
+    public Pipe(int x, int y, int width, int height, Image img) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.img = img;
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(img, x, y, width, height, null);
+    }
+}
