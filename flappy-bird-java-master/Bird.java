d516d01de2bd1a8c84b94421983d9ca48e3bc610
Zerzo15 <vvmquang1510@gmail.com>
2025-05-15 23:40:08 +0700

    Create Bird.java

---
+++ flappy-bird-java-master/Bird.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class Bird {
+    public int x, y, width, height;
+    public Image img;
+
+    public Bird(int x, int y, int width, int height, Image img) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.img = img;
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(img, x, y, width, height, null);
+    }
+}
