c644f9576642f888499517e33d4f070417da0c4e
Zerzo15 <vvmquang1510@gmail.com>
2025-06-05 22:12:59 +0700

    Update Pipe.java

--- flappy-bird-java-master/Pipe.java
+++ flappy-bird-java-master/Pipe.java
@@ -1,20 +1,15 @@
 import java.awt.Graphics;
 import java.awt.Image;
 
-public class Pipe {
-    public int x, y, width, height;
-    public Image img;
+public class Pipe extends GameObject {
     public boolean passed = false;
     private float velocityX = -3;
 
     public Pipe(int x, int y, int width, int height, Image img) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.img = img;
+        super(x, y, width, height, img);
     }
 
+    @Override
     public void draw(Graphics g) {
         g.drawImage(img, x, y, width, height, null);
     }
@@ -23,4 +18,3 @@ public class Pipe {
         x += velocityX;
     }
 }
-
