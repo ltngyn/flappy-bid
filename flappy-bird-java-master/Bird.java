3a8216f221a1bee95bdf9bad208b1070ded5a522
Zerzo15 <vvmquang1510@gmail.com>
2025-06-05 22:12:46 +0700

    Update Bird.java

--- flappy-bird-java-master/Bird.java
+++ flappy-bird-java-master/Bird.java
@@ -1,33 +1,27 @@
 import java.awt.Graphics;
 import java.awt.Image;
 
-public class Bird {
-    public int x, y, width, height;
-    public Image img;
+public class Bird extends GameObject {
     public double velocityY = 0;
     public double gravity = 0.8;
 
-    private double idleTime = 0; // used for animation
-    private int startY; // original position to bob around
-
+    private double idleTime = 0;
+    private int startY;
 
     public Bird(int x, int y, int width, int height, Image img) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.img = img;
+        super(x, y, width, height, img);
         this.startY = y;
     }
 
+    @Override
     public void draw(Graphics g) {
         g.drawImage(img, x, y, width, height, null);
     }
 
     public void idleUpdate() {
-        idleTime += 0.1; // adjust for speed
-        y = startY + (int)(5 * Math.sin(idleTime)); // 5px up/down swing
-    }   
+        idleTime += 0.1;
+        y = startY + (int)(5 * Math.sin(idleTime));
+    }
 
     public void update() {
         velocityY += gravity;
